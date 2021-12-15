40497a94491477967d8893f86fa75c04fd323f22
Jonathan Baraza <jbaraza46@gmail.com>
2021-12-15 14:10:50 +0300

    initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../App.css";
+const Button = () => {
+    return (
+        <div>
+             <button className='button'>CREATE ACCOUNT</button>
+
+             <style jsx>
+                 
+            </style>
+        </div>
+
+        
+    )
+}
+
+
+
+export default Button
